93fa99f1599d559e00386fdbf2b44454c5b9a515
Kent Harris <kentharris13@gmail.com>
2022-01-05 13:18:36 -0700

    deployu

--- server/index.js
+++ server/index.js
@@ -9,12 +9,22 @@ const { favoriteList, getProducts, saveFavorite } = require('./controller.js')
 app.use(express.json())
 app.use(cors())
 
+//Added to try and get this to deploy
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+
+
 // app.get("/", (req, res) => {res.send("Hello World")})
 
 app.post("/api/user",controller.postUsers)
 app.get('/products', getProducts)
 app.put('/favorite', favoriteList)
 app.post('/saveFavorite/:id', saveFavorite)
+
+//added to try to get this to deploy
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build' 'index.html'));
+})
 
 
 app.listen(PORT,() => {console.log(`We have our headding to ${PORT}`)})
